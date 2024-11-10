9135cd22c6e418d0efada7e97593667e11d2c4bc
Sachin <sachinkaushikofficial@gmail.com>
2024-11-10 16:57:13 +0530

    initial commit

---
+++ src/Sudoku/SudokuMain.java
@@ -0,0 +1,109 @@
+package Sudoku;
+
+public class SudokuMain {
+
+    //Apna College - Shraddha Kapra didi
+    public static void main(String[] args) {
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+
+        System.out.println("Welcome to Sudoku Solver!");
+
+        if (validSudokuSolution(board, 0, 0)) {
+            System.out.println("Successfully solved the board");
+        } else {
+            System.out.println("Unsolvable board :(");
+        }
+
+        printBoard(board);
+
+//        System.out.println(validSudokuSolution(board, 0, 0));
+        }
+
+
+        public static void printBoard(char[][] board) {
+            for (int i=0; i<board.length; i++) {
+                for (int j=0; j<board.length; j++) {
+                    System.out.print(board[i][j]);
+                }
+                System.out.println();
+            }
+        }
+
+        //recursive function which will return boolean
+    public static boolean validSudokuSolution(char[][] board, int row, int column) {
+        //base condition
+        if (row==board.length) {
+            return true;
+        }
+
+        //from next time, what will be out new row and column?
+        int newRow = 0;
+        int newColumn = 0;
+        if (column != board.length - 1) {
+            newRow = row;
+            newColumn = column + 1;
+        } else {
+            newRow = row + 1;
+            newColumn = 0;
+        }
+        // if already number is written then move to next cell. OR else check to set one of nos from 1 to 9 numbers
+        if (board[row][column] != '.') {
+            if (validSudokuSolution(board, newRow, newColumn)) {
+                return true;    // we have got the solution
+            }
+        } else {
+            for (int i = 1; i<=board.length; i++) {
+                if (isSafe(board, row, column, i)) {
+                    board[row][column] = (char) (i+'0');
+                    if (validSudokuSolution(board, newRow, newColumn)) {
+                        return true;
+                    } else {
+                        board[row][column] = '.';
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isSafe(char[][] board, int row, int column, int number) {
+        //to verify if it is safe to place the number or NOT
+        for (int i =0; i<board.length; i++) {
+            if (board[i][column] == (char)(number + '0')) {
+                return false;
+            }
+            if (board[row][i] == (char)(number + '0')) {
+                return false;
+            }
+        }
+
+        //grid
+        int startRow = (row/3) * 3;
+        int startColumn = (column/3) * 3;
+
+        for (int i = row; i < startRow+3; i++) {
+            for (int j = column; j < startColumn+3; j++) {
+                if (board[i][j] == (char)(number + '0')) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void solveSudoku(char[][] board) {
+        validSudokuSolution(board, 0, 0);
+
+    }
+}
---
+++ src/Sudoku/SudokuSolverApproach2.java
@@ -0,0 +1,118 @@
+package Sudoku;
+
+public class SudokuSolverApproach2 {
+
+    //Code with John
+
+    private static final int GRID_SIZE = 9;
+    public static void main(String[] args) {
+
+        int[][] board = {
+                {7, 0, 2, 0, 5, 0, 6, 0, 0},
+                {0, 0, 0, 0, 0, 3, 0, 0, 0},
+                {1, 0, 0, 0, 0, 9, 5, 0, 0},
+                {8, 0, 0, 0, 0, 0, 0, 9, 0},
+                {0, 4, 3, 0, 0, 0, 7, 5, 0},
+                {0, 9, 0, 0, 0, 0, 0, 0, 8},
+                {0, 0, 9, 7, 0, 0, 0, 0, 5},
+                {0, 0, 0, 2, 0, 0, 0, 0, 0},
+                {0, 0, 7, 0, 4, 0, 2, 0, 3}
+        };
+
+        System.out.println("Welcome to Sudoku Solver Program");
+
+        printBoard(board);
+        if (solveBoard(board)) {
+            System.out.println("Successfully solved the board");
+        } else {
+            System.out.println("Unsolvable board :(");
+        }
+        System.out.println("Board after solving--");
+        printBoard(board);
+    }
+
+
+    private static void printBoard(int[][] board) {
+        for (int row=0; row<board.length; row++) {
+            if (row % 3 == 0 && row != 0) {
+                System.out.println("-----------");
+            }
+            for (int column=0; column<board.length; column++) {
+                if (column % 3 == 0 && column != 0) {
+                    System.out.print("|");
+                }
+                System.out.print(board[row][column]);
+            }
+            System.out.println();
+        }
+    }
+
+    //method to check if that number already exists in the row
+    private static boolean numberExistInRow (int[][] board, int number, int row) {
+        for (int i=0; i < GRID_SIZE; i++) {
+            if (board[row][i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean numberExistInColumn (int[][] board, int number, int column) {
+        for (int i=0; i < GRID_SIZE; i++) {
+            if (board[i][column] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // check if number exists in the 3x3 grid
+    private static boolean numberExistInGrid (int[][] board, int number, int row, int column) {
+        int localBoxRow = row - (row % 3);
+        int localBoxCol = column - (column % 3);
+
+        for (int i= localBoxRow; i< localBoxRow+3; i++) {
+            for (int j= localBoxCol; j< localBoxCol+3; j++) {
+                if(board[i][j] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //Lets write one method that checks all 3 of those above conditions
+    private static boolean isValidPlacement(int[][] board, int number, int row, int column) {
+        return !numberExistInRow(board,number,row) &&
+                !numberExistInColumn(board,number,column) &&
+                !numberExistInGrid(board,number,row,column);
+    }
+
+    //real method/algorithm which does all recursion & backtracking
+    private static boolean solveBoard(int[][] board) {
+        for (int row=0; row < GRID_SIZE; row++) {
+            for (int column=0; column < GRID_SIZE; column++) {
+                if (board[row][column] == 0) {
+                    //try all numbers from 1 to 9..
+                    for (int numberToTry=1; numberToTry <= GRID_SIZE; numberToTry++) {
+                        if (isValidPlacement(board,numberToTry,row,column)) {
+                            board[row][column] = numberToTry;
+
+                            //here is where recursion of algo comes in
+                            if(solveBoard(board)){
+                                return true;
+                            } else {
+                                // clear the placement of number we tried and put 0 back again (signifies empty)
+                                board[row][column] = 0;
+                            }
+                        }
+                    }
+                    //none of the number is valid placement then return false -> i.e I can't solve this board
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
